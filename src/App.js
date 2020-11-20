12ea8e76e8d6c5032f05c37ee0eebeb9b0fa33e7
nostam <matsonho@gmail.com>
2020-11-20 11:43:52 +0100

    add footer with rita's code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Modal } from 'react-bootstrap';
-import './App.css';
-import MovieList from './components/MovieList'
-import ModalMovie from './components/ModalMovie'
-
+import { Modal } from "react-bootstrap";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import ModalMovie from "./components/ModalMovie";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <MovieList />
-      <ModalMovie/>
+      <ModalMovie />
+      <Footer />
     </div>
   );
 }
